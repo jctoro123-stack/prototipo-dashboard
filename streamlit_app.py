9f869fdbf92e9b2f194d769a97e5fbca220c658c
jctoro123-stack <jctoro123@gmail.com>
2026-04-21 16:03:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,9 +92,6 @@ with col2:
             "imc": [imc]
 })
 
-# Reordenar según el scaler
-datos = datos[scaler.feature_names_in_]
-
 datos_scaled = scaler.transform(datos)
 
 prob = modelo.predict_proba(datos_scaled)[0][1]
